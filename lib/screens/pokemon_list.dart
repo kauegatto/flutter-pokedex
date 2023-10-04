7ef9af888b7778d6a05e47514ac1bdf5192dbc37
kaue gatto <kauegatto123@gmail.com>
2023-10-04 19:41:13 -0300

    refactor: store future inside state

--- lib/screens/pokemon_list.dart
+++ lib/screens/pokemon_list.dart
@@ -8,15 +8,28 @@ import '../logic/login/login_state.dart';
 import '../model/pokemon_model.dart';
 import '../widgets/pokemon_list_item.dart';
 
-class PokedexScreen extends StatelessWidget {
+class PokedexScreen extends StatefulWidget {
   const PokedexScreen({Key? key}) : super(key: key);
+
+  @override
+  PokedexScreenState createState() => PokedexScreenState();
+}
+
+class PokedexScreenState extends State<PokedexScreen> {
+  late Future<List<PokemonModel>> _pokemonListFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _pokemonListFuture = PokemonModel.getPokemonList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<LoginBloc, LoginState>(
       builder: (context, loginState) {
         return FutureBuilder<List<PokemonModel>>(
-          future: PokemonModel.getPokemonList(),
+          future: _pokemonListFuture, // Use the stored future
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: CircularProgressIndicator());
