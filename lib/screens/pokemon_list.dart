04f1b4c9d84566ddcc0bf7460abd9b0dcc77b9a7
kaue gatto <kauegatto123@gmail.com>
2023-10-03 21:21:58 -0300

    refactor: move widget logic inside a widget container for pokemon list item

--- lib/screens/pokemon_list.dart
+++ lib/screens/pokemon_list.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../logic/like/like_bloc.dart';
-import '../logic/like/like_event.dart';
 import '../logic/like/like_state.dart';
 import '../logic/login/login_bloc.dart';
 import '../logic/login/login_state.dart';
 import '../model/pokemon_model.dart';
+import '../widgets/pokemon_list_item.dart';
 
 class PokedexScreen extends StatelessWidget {
   PokedexScreen({Key? key}) : super(key: key);
 
   final List<PokemonModel> pokemonList = PokemonModel.getPokemon();
-
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<LoginBloc, LoginState>(
@@ -28,28 +27,9 @@ class PokedexScreen extends StatelessWidget {
                 final pokemon = pokemonList[index];
                 return BlocBuilder<LikeBloc, LikeState>(
                   builder: (context, likeState) {
-                    return ListTile(
-                      leading: Image.asset(
-                        pokemon.image,
-                        width: 50,
-                        height: 50,
-                      ),
-                      title: Text(pokemon.name),
-                      subtitle: Text('Tipo: ${pokemon.type}'),
-                      trailing: IconButton(
-                        icon: Icon(
-                          likeState.isLiked(pokemon.number)
-                              ? Icons.favorite
-                              : Icons.favorite_border,
-                          color: pokemon.liked ? Colors.red : null,
-                        ),
-                        onPressed: () {
-                          _toggleLike(pokemon, context);
-                        },
-                      ),
-                      onTap: () {
-                        _showPokemonDetails(context, pokemon);
-                      },
+                    return PokemonListItem(
+                      pokemon: pokemon,
+                      likeState: likeState,
                       tileColor: index % 2 == 0
                           ? const Color.fromARGB(183, 44, 40, 40)
                           : const Color.fromARGB(255, 133, 20, 27),
@@ -63,64 +43,4 @@ class PokedexScreen extends StatelessWidget {
       },
     );
   }
-}
-
-void _toggleLike(PokemonModel pokemon, BuildContext context) {
-  final likeBloc = BlocProvider.of<LikeBloc>(context);
-  likeBloc.add(LikeEvent(pokemon.number));
-  _showLikeSnackBar(context, pokemon);
-}
-
-void _showLikeSnackBar(BuildContext context, PokemonModel pokemon) {
-  final snackBar = SnackBar(
-    content: Text(pokemon.liked
-        ? 'Curtiu ${pokemon.name} ❤️'
-        : 'Descurtiu ${pokemon.name} 😭'),
-    action: SnackBarAction(
-      label: 'Desfazer',
-      onPressed: () {
-        _toggleLike(pokemon, context);
-      },
-    ),
-  );
-  ScaffoldMessenger.of(context).removeCurrentSnackBar();
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
-
-void _showPokemonDetails(BuildContext context, PokemonModel pokemon) {
-  showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: Text(pokemon.name),
-        content: SingleChildScrollView(
-          // Vamos comocar o filho como column e permitir que ele seja scrollado
-          child: Column(
-            mainAxisSize: MainAxisSize
-                .min, // Usar mainAxisSize.min pra nao ocupar espaço desnecessario, o mesmo que em home
-            children: [
-              Image.asset(
-                pokemon.image,
-                width: 150,
-                height: 150,
-              ),
-              Text('Número na pokedex: #${pokemon.number}'),
-              Text('Tipo: ${pokemon.type}'),
-              if (pokemon.curiosities != null)
-                Text('Curiosidade: ${pokemon.curiosities}'),
-            ],
-          ),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child:
-                const Text('Fechar', style: TextStyle(color: Colors.redAccent)),
-          ),
-        ],
-      );
-    },
-  );
 }
---
+++ lib/widgets/pokemon_list_item.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../logic/like/like_bloc.dart';
+import '../logic/like/like_event.dart';
+import '../logic/like/like_state.dart';
+import '../model/pokemon_model.dart';
+
+class PokemonListItem extends StatelessWidget {
+  final PokemonModel pokemon;
+  final LikeState likeState;
+  final Color tileColor;
+
+  const PokemonListItem(
+      {super.key,
+      required this.pokemon,
+      required this.likeState,
+      this.tileColor = Colors.red});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.asset(
+        pokemon.image,
+        width: 50,
+        height: 50,
+      ),
+      title: Text(pokemon.name),
+      subtitle: Text('Tipo: ${pokemon.type}'),
+      trailing: IconButton(
+        icon: Icon(
+          likeState.isLiked(pokemon.number)
+              ? Icons.favorite
+              : Icons.favorite_border,
+          color: pokemon.liked ? Colors.red : null,
+        ),
+        onPressed: () {
+          _toggleLike(pokemon, context);
+        },
+      ),
+      onTap: () {
+        _showPokemonDetails(context, pokemon);
+      },
+      tileColor: tileColor,
+    );
+  }
+
+  void _showPokemonDetails(BuildContext context, PokemonModel pokemon) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(pokemon.name),
+          content: SingleChildScrollView(
+            // Vamos comocar o filho como column e permitir que ele seja scrollado
+            child: Column(
+              mainAxisSize: MainAxisSize
+                  .min, // Usar mainAxisSize.min pra nao ocupar espaço desnecessario, o mesmo que em home
+              children: [
+                Image.asset(
+                  pokemon.image,
+                  width: 150,
+                  height: 150,
+                ),
+                Text('Número na pokedex: #${pokemon.number}'),
+                Text('Tipo: ${pokemon.type}'),
+                if (pokemon.curiosities != null)
+                  Text('Curiosidade: ${pokemon.curiosities}'),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar',
+                  style: TextStyle(color: Colors.redAccent)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+void _toggleLike(PokemonModel pokemon, BuildContext context) {
+  final likeBloc = BlocProvider.of<LikeBloc>(context);
+  likeBloc.add(LikeEvent(pokemon.number));
+  _showLikeSnackBar(context, pokemon);
+}
+
+void _showLikeSnackBar(BuildContext context, PokemonModel pokemon) {
+  final snackBar = SnackBar(
+    content: Text(pokemon.liked
+        ? 'Curtiu ${pokemon.name} ❤️'
+        : 'Descurtiu ${pokemon.name} 😭'),
+    action: SnackBarAction(
+      label: 'Desfazer',
+      onPressed: () {
+        _toggleLike(pokemon, context);
+      },
+    ),
+  );
+  ScaffoldMessenger.of(context).removeCurrentSnackBar();
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
