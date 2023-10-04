eee1974d4701f0bb7d9bb404279becb27adaa147
kaue gatto <kauegatto123@gmail.com>
2023-10-04 19:51:20 -0300

    Revert "refactor: store future inside state"
    This reverts commit 7ef9af888b7778d6a05e47514ac1bdf5192dbc37.

--- lib/screens/liked_pokemon.dart
+++ lib/screens/liked_pokemon.dart
@@ -9,76 +9,66 @@ import '../logic/login/login_bloc.dart';
 import '../logic/login/login_state.dart';
 import '../widgets/pokemon_list_item.dart';
 
-class LikedPokemonScreen extends StatefulWidget {
+class LikedPokemonScreen extends StatelessWidget {
   const LikedPokemonScreen({Key? key}) : super(key: key);
 
   @override
-  LikedPokemonScreenState createState() => LikedPokemonScreenState();
-}
-
-class LikedPokemonScreenState extends State<LikedPokemonScreen> {
-  late Future<List<PokemonModel>> _likedPokemonFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    final email = (BlocProvider.of<LoginBloc>(context).state is LoggedIn)
-        ? (BlocProvider.of<LoginBloc>(context).state as LoggedIn).email
-        : null;
-    final likeBloc = BlocProvider.of<LikeBloc>(context);
-
-    // Dispatch the FindLikedEvent to fetch liked Pokémon
-    likeBloc.add(FindLikedEvent(email));
-    _likedPokemonFuture =
-        PokemonModel.getPokemonByNumbers(likeBloc.state.pokemonLikes);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<LikeBloc, LikeState>(
-        builder: (context, likeState) {
-          return FutureBuilder<List<PokemonModel>>(
-            future: _likedPokemonFuture, // Use the stored future
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                final likedPokemon = snapshot.data;
-                // Build your screen based on the fetched liked Pokémon data
-                if (likedPokemon!.isEmpty) {
-                  return const Center(
-                    child: Text(
-                      "Você ainda não curtiu nenhum Pokémon!",
-                      maxLines: 1,
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        letterSpacing: 2,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  );
-                }
-                return ListView.builder(
-                  itemCount: likedPokemon.length,
-                  itemBuilder: (context, index) {
-                    final pokemon = likedPokemon[index];
-                    return PokemonListItem(
-                      pokemon: pokemon,
-                      likeState: likeState,
-                      tileColor: index % 2 == 0
-                          ? const Color.fromARGB(183, 44, 40, 40)
-                          : const Color.fromARGB(255, 133, 20, 27),
+    return BlocBuilder<LoginBloc, LoginState>(
+      builder: (context, loginState) {
+        final email = (loginState is LoggedIn) ? loginState.email : null;
+        final likeBloc = BlocProvider.of<LikeBloc>(context);
+
+        // Dispatch the FindLikedEvent to fetch liked Pokémon
+        likeBloc.add(FindLikedEvent(email));
+
+        return Scaffold(
+          body: BlocBuilder<LikeBloc, LikeState>(
+            builder: (context, likeState) {
+              return FutureBuilder<List<PokemonModel>>(
+                future:
+                    PokemonModel.getPokemonByNumbers(likeState.pokemonLikes),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    final likedPokemon = snapshot.data;
+                    // Build your screen based on the fetched liked Pokémon data
+                    if (likedPokemon!.isEmpty) {
+                      return const Center(
+                        child: Text(
+                          "Você ainda não curtiu nenhum Pokémon!",
+                          maxLines: 1,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      );
+                    }
+                    return ListView.builder(
+                      itemCount: likedPokemon.length,
+                      itemBuilder: (context, index) {
+                        final pokemon = likedPokemon[index];
+                        return PokemonListItem(
+                          pokemon: pokemon,
+                          likeState: likeState,
+                          tileColor: index % 2 == 0
+                              ? const Color.fromARGB(183, 44, 40, 40)
+                              : const Color.fromARGB(255, 133, 20, 27),
+                        );
+                      },
                     );
-                  },
-                );
-              }
+                  }
+                },
+              );
             },
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/screens/pokemon_list.dart
+++ lib/screens/pokemon_list.dart
@@ -8,28 +8,15 @@ import '../logic/login/login_state.dart';
 import '../model/pokemon_model.dart';
 import '../widgets/pokemon_list_item.dart';
 
-class PokedexScreen extends StatefulWidget {
+class PokedexScreen extends StatelessWidget {
   const PokedexScreen({Key? key}) : super(key: key);
-
-  @override
-  PokedexScreenState createState() => PokedexScreenState();
-}
-
-class PokedexScreenState extends State<PokedexScreen> {
-  late Future<List<PokemonModel>> _pokemonListFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _pokemonListFuture = PokemonModel.getPokemonList();
-  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<LoginBloc, LoginState>(
       builder: (context, loginState) {
         return FutureBuilder<List<PokemonModel>>(
-          future: _pokemonListFuture, // Use the stored future
+          future: PokemonModel.getPokemonList(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: CircularProgressIndicator());
