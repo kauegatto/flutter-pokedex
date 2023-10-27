e5c0bd90242e25159fc27d97469086f4627b91af
Kaue Gatto <kauegatto123@gmail.com>
2023-10-27 20:09:10 -0300

    to entendendo mais nada (nem o professor)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dex/logic/login/login_bloc.dart';
 import 'package:flutter_dex/screens/wrapper.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
